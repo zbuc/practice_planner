1fe341a96d5f14a5c730e665c02c8534e355e289
Chris Czub <chris@penumbra.zone>
2021-12-25 14:28:23 -0500

    yew app

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,24 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
+use std::{thread, time};
 
 use anyhow::Result;
 use chrono::{Date, DateTime, Duration, Utc};
+use gloo::storage::{LocalStorage, Storage};
 use rand::prelude::*;
 use rand::seq::SliceRandom;
-use rodio::Source;
-use rodio::{Decoder, OutputStream};
 use serde::{Deserialize, Serialize};
 use text_io::read;
 use thiserror::Error;
-use tokio::time;
+use web_sys::HtmlInputElement as InputElement;
+use yew::{
+    classes,
+    events::{FocusEvent, KeyboardEvent},
+    html,
+    html::Scope,
+    Classes, Component, Context, Html, NodeRef, TargetCast,
+};
 
 lazy_static! {
     pub(crate) static ref DEFAULT_CATEGORIES: Vec<PracticeCategory> = vec![
@@ -48,6 +55,8 @@ lazy_static! {
         },
     ];
 }
+
+const KEY: &str = "yew.todomvc.self";
 
 #[derive(Error, Debug)]
 pub enum SchedulerError {
@@ -92,7 +101,7 @@ pub struct SchedulePlanner {
 impl SchedulePlanner {
     pub fn new() -> Self {
         // Call this here so it's cached and faster later
-        let (_stream, _stream_handle) = OutputStream::try_default().unwrap();
+        // let (_stream, _stream_handle) = OutputStream::try_default().unwrap();
         SchedulePlanner {
             //category_practice_time: Duration::minutes(15),
             category_practice_time: Duration::seconds(1),
@@ -209,42 +218,43 @@ impl SchedulePlanner {
         return Ok(());
     }
 
-    pub async fn start_category(&self, category: &PracticeCategory) -> Result<()> {
+    pub fn start_category(&self, category: &PracticeCategory) -> Result<()> {
         println!(
             "Starting {} minute practice for category: {:#?}",
             self.category_practice_time.num_minutes(),
             category
         );
-        time::sleep(self.category_practice_time.to_std()?).await;
+
+        thread::sleep(self.category_practice_time.to_std()?);
         println!("Done practicing category: {:#?}", category);
-        self.play_ding_sound().await?;
+        // self.play_ding_sound();
 
         Ok(())
     }
 
-    pub async fn play_ding_sound(&self) -> Result<()> {
+    pub fn play_ding_sound(&self) -> Result<()> {
         // TODO these should be singletons or maybe the Sink type idk or a separate audio player task
         // Get a output stream handle to the default physical sound device
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        // Load a sound from a file, using a path relative to Cargo.toml
-        let file = BufReader::new(File::open("audio/ding.wav").unwrap());
-        // Decode that sound file into a source
-        let source = Decoder::new(file).unwrap();
+        // let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        // // Load a sound from a file, using a path relative to Cargo.toml
+        // let file = BufReader::new(File::open("audio/ding.wav").unwrap());
+        // // Decode that sound file into a source
+        // let source = Decoder::new(file).unwrap();
 
-        stream_handle.play_raw(source.convert_samples())?;
+        // stream_handle.play_raw(source.convert_samples())?;
 
         // need to sleep for the duration of the ding sound
         // (todo should probably move to a second thread/task/whatever in tokio)
-        time::sleep(std::time::Duration::from_secs(2)).await;
+        thread::sleep(std::time::Duration::from_secs(2));
         Ok(())
     }
 
-    pub async fn start_daily_practice(&mut self) -> Result<()> {
+    pub fn start_daily_practice(&mut self) -> Result<()> {
         // ensure today's schedule has been set
         self.update_todays_schedule(false)?;
         for category in self.todays_schedule.as_ref().unwrap().iter() {
             println!("Starting practice for category: {:#?}", category);
-            self.start_category(&category).await?;
+            self.start_category(&category)?;
         }
         println!("Finished practicing for today!");
 
@@ -291,8 +301,240 @@ impl SchedulePlanner {
     }
 }
 
-#[tokio::main]
-async fn main() {
+pub enum Msg {
+    Add(String),
+    Edit((usize, String)),
+    Remove(usize),
+    ToggleAll,
+    ToggleEdit(usize),
+    Toggle(usize),
+    ClearCompleted,
+    Focus,
+}
+
+pub struct Model {
+    // state: State,
+    focus_ref: NodeRef,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        // let entries = LocalStorage::get(KEY).unwrap_or_else(|_| Vec::new());
+        // let state = State {
+        //     entries,
+        //     filter: Filter::All,
+        //     edit_value: "".into(),
+        // };
+        let focus_ref = NodeRef::default();
+        // Self { state, focus_ref }
+        Self { focus_ref }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Add(description) => {
+                if !description.is_empty() {
+                    // let entry = Entry {
+                    //     description: description.trim().to_string(),
+                    //     completed: false,
+                    //     editing: false,
+                    // };
+                    // self.state.entries.push(entry);
+                }
+            }
+            Msg::Edit((idx, edit_value)) => {
+                // self.state.complete_edit(idx, edit_value.trim().to_string());
+                // self.state.edit_value = "".to_string();
+            }
+            Msg::Remove(idx) => {
+                // self.state.remove(idx);
+            }
+            // Msg::SetFilter(filter) => {
+            //     self.state.filter = filter;
+            // }
+            Msg::ToggleEdit(idx) => {
+                // self.state.edit_value = self.state.entries[idx].description.clone();
+                // self.state.clear_all_edit();
+                // self.state.toggle_edit(idx);
+            }
+            Msg::ToggleAll => {
+                // let status = !self.state.is_all_completed();
+                // self.state.toggle_all(status);
+            }
+            Msg::Toggle(idx) => {
+                // self.state.toggle(idx);
+            }
+            Msg::ClearCompleted => {
+                // self.state.clear_completed();
+            }
+            Msg::Focus => {
+                // if let Some(input) = self.focus_ref.cast::<InputElement>() {
+                //     input.focus().unwrap();
+                // }
+            }
+        }
+        // LocalStorage::set(KEY, &self.state.entries).expect("failed to set");
+        true
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let hidden_class = "hidden";
+        // let hidden_class = if self.state.entries.is_empty() {
+        //     "hidden"
+        // } else {
+        //     ""
+        // };
+        html! {
+            <div class="todomvc-wrapper">
+                <section class="todoapp">
+                    <header class="header">
+                        <h1>{ "todos" }</h1>
+                        { self.view_input(ctx.link()) }
+                    </header>
+                    <section class={classes!("main", hidden_class)}>
+                        <input
+                            type="checkbox"
+                            class="toggle-all"
+                            id="toggle-all"
+                            // checked={self.state.is_all_completed()}
+                            onclick={ctx.link().callback(|_| Msg::ToggleAll)}
+                        />
+                        <label for="toggle-all" />
+                        <ul class="todo-list">
+                            // { for self.state.entries.iter().filter(|e| self.state.filter.fits(e)).enumerate().map(|e| self.view_entry(e, ctx.link())) }
+                        </ul>
+                    </section>
+                    <footer class={classes!("footer", hidden_class)}>
+                        <span class="todo-count">
+                            // <strong>{ self.state.total() }</strong>
+                            { " item(s) left" }
+                        </span>
+                        <ul class="filters">
+                            // { for Filter::iter().map(|flt| self.view_filter(flt, ctx.link())) }
+                        </ul>
+                        <button class="clear-completed" onclick={ctx.link().callback(|_| Msg::ClearCompleted)}>
+                            // { format!("Clear completed ({})", self.state.total_completed()) }
+                        </button>
+                    </footer>
+                </section>
+                <footer class="info">
+                    <p>{ "Double-click to edit a todo" }</p>
+                    <p>{ "Written by " }<a href="https://github.com/DenisKolodin/" target="_blank">{ "Denis Kolodin" }</a></p>
+                    <p>{ "Part of " }<a href="http://todomvc.com/" target="_blank">{ "TodoMVC" }</a></p>
+                </footer>
+            </div>
+        }
+    }
+}
+
+impl Model {
+    // fn view_filter(&self, filter: Filter, link: &Scope<Self>) -> Html {
+    //     let cls = if self.state.filter == filter {
+    //         "selected"
+    //     } else {
+    //         "not-selected"
+    //     };
+    //     html! {
+    //         <li>
+    //             <a class={cls}
+    //                href={filter.as_href()}
+    //                onclick={link.callback(move |_| Msg::SetFilter(filter))}
+    //             >
+    //                 { filter }
+    //             </a>
+    //         </li>
+    //     }
+    // }
+
+    fn view_input(&self, link: &Scope<Self>) -> Html {
+        let onkeypress = link.batch_callback(|e: KeyboardEvent| {
+            if e.key() == "Enter" {
+                let input: InputElement = e.target_unchecked_into();
+                let value = input.value();
+                input.set_value("");
+                Some(Msg::Add(value))
+            } else {
+                None
+            }
+        });
+        html! {
+            // You can use standard Rust comments. One line:
+            // <li></li>
+            <input
+                class="new-todo"
+                placeholder="What needs to be done?"
+                {onkeypress}
+            />
+            /* Or multiline:
+            <ul>
+                <li></li>
+            </ul>
+            */
+        }
+    }
+
+    // fn view_entry(&self, (idx, entry): (usize, &Entry), link: &Scope<Self>) -> Html {
+    //     let mut class = Classes::from("todo");
+    //     if entry.editing {
+    //         class.push(" editing");
+    //     }
+    //     if entry.completed {
+    //         class.push(" completed");
+    //     }
+    //     html! {
+    //         <li {class}>
+    //             <div class="view">
+    //                 <input
+    //                     type="checkbox"
+    //                     class="toggle"
+    //                     checked={entry.completed}
+    //                     onclick={link.callback(move |_| Msg::Toggle(idx))}
+    //                 />
+    //                 <label ondblclick={link.callback(move |_| Msg::ToggleEdit(idx))}>{ &entry.description }</label>
+    //                 <button class="destroy" onclick={link.callback(move |_| Msg::Remove(idx))} />
+    //             </div>
+    //             { self.view_entry_edit_input((idx, entry), link) }
+    //         </li>
+    //     }
+    // }
+
+    // fn view_entry_edit_input(&self, (idx, entry): (usize, &Entry), link: &Scope<Self>) -> Html {
+    //     let edit = move |input: InputElement| {
+    //         let value = input.value();
+    //         input.set_value("");
+    //         Msg::Edit((idx, value))
+    //     };
+
+    //     let onblur = link.callback(move |e: FocusEvent| edit(e.target_unchecked_into()));
+
+    //     let onkeypress = link.batch_callback(move |e: KeyboardEvent| {
+    //         (e.key() == "Enter").then(|| edit(e.target_unchecked_into()))
+    //     });
+
+    //     if entry.editing {
+    //         html! {
+    //             <input
+    //                 class="edit"
+    //                 type="text"
+    //                 ref={self.focus_ref.clone()}
+    //                 value={self.state.edit_value.clone()}
+    //                 onmouseover={link.callback(|_| Msg::Focus)}
+    //                 {onblur}
+    //                 {onkeypress}
+    //             />
+    //         }
+    //     } else {
+    //         html! { <input type="hidden" /> }
+    //     }
+    // }
+}
+
+fn main() {
+    yew::start_app::<Model>();
+
     let mut scheduler = match Path::new("./saved_data/history.bin").exists() {
         true => {
             println!("Saved data found, loading...");
@@ -321,7 +563,6 @@ async fn main() {
             println!("Yeehaw");
             scheduler
                 .start_daily_practice()
-                .await
                 .expect("Unable to run daily practice");
         }
         _ => {
